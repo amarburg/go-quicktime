c4ab625e264909d37d97be9a6979c06c87ed6b02
Aaron Marburg <amarburg@apl.washington.edu>
2017-01-17 13:58:03 -0800

    Add command line flag to handle eagerloading.

--- cmd/dump_mov_tree/dump_mov_tree.go
+++ cmd/dump_mov_tree/dump_mov_tree.go
@@ -3,16 +3,30 @@ package main
 import "os"
 import "fmt"
 import "github.com/amarburg/go-quicktime"
-
+import "flag"
+import "strings"
 
 func main() {
 
-  if len(os.Args) < 2 {
+  el := flag.String("eagerload","","Atom types to eagerload")
+
+  flag.Parse();
+
+  atoms := strings.Split(*el, ",")
+  eagerLoadFunc := func ( conf *quicktime.BuildTreeConfig ) {
+    if len(atoms) > 0 {
+      conf.EagerloadTypes = atoms
+    }
+  }
+
+
+  // Now handle the remaining args
+  if len(flag.Args()) == 0 {
     fmt.Printf("Usage:   dump_mov_tree [MOV files]\n")
     os.Exit(-1)
   }
 
-  for _,filename := range os.Args[1:] {
+  for _,filename := range flag.Args() {
     file,err := os.Open( filename )
     if err != nil {
       fmt.Printf("Couldn't open file %s: %s\n", filename, err.Error() )
@@ -21,7 +35,7 @@ func main() {
 
     fileinfo,_ := file.Stat()
 
-    tree,err := quicktime.BuildTree( file, fileinfo.Size() )
+    tree,err := quicktime.BuildTree( file, fileinfo.Size(), eagerLoadFunc )
     if err != nil {
       fmt.Printf("Error loading atom tree for %s: %s\n", filename, err.Error() )
     }
