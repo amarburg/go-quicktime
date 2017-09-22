b010cc4f43f2e315bf1ca10846dbdb2a94492c3e
Aaron Marburg <amarburg@apl.washington.edu>
2017-09-21 20:19:46 -0800

    Fixed int64 - uint64 mismatch in dump_mov_tree

--- cmd/dump_mov_tree/dump_mov_tree.go
+++ cmd/dump_mov_tree/dump_mov_tree.go
@@ -8,38 +8,37 @@ import "strings"
 
 func main() {
 
-  el := flag.String("eagerload","","Atom types to eagerload")
+	el := flag.String("eagerload", "", "Atom types to eagerload")
 
-  flag.Parse();
+	flag.Parse()
 
-  atoms := strings.Split(*el, ",")
-  eagerLoadFunc := func ( conf *quicktime.BuildTreeConfig ) {
-    if len(atoms) > 0 {
-      conf.EagerloadTypes = atoms
-    }
-  }
+	atoms := strings.Split(*el, ",")
+	eagerLoadFunc := func(conf *quicktime.BuildTreeConfig) {
+		if len(atoms) > 0 {
+			conf.EagerloadTypes = atoms
+		}
+	}
 
+	// Now handle the remaining args
+	if len(flag.Args()) == 0 {
+		fmt.Printf("Usage:   dump_mov_tree [MOV files]\n")
+		os.Exit(-1)
+	}
 
-  // Now handle the remaining args
-  if len(flag.Args()) == 0 {
-    fmt.Printf("Usage:   dump_mov_tree [MOV files]\n")
-    os.Exit(-1)
-  }
+	for _, filename := range flag.Args() {
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Printf("Couldn't open file %s: %s\n", filename, err.Error())
+			continue
+		}
 
-  for _,filename := range flag.Args() {
-    file,err := os.Open( filename )
-    if err != nil {
-      fmt.Printf("Couldn't open file %s: %s\n", filename, err.Error() )
-      continue
-    }
+		fileinfo, _ := file.Stat()
 
-    fileinfo,_ := file.Stat()
+		tree, err := quicktime.BuildTree(file, uint64(fileinfo.Size()), eagerLoadFunc)
+		if err != nil {
+			fmt.Printf("Error loading atom tree for %s: %s\n", filename, err.Error())
+		}
 
-    tree,err := quicktime.BuildTree( file, fileinfo.Size(), eagerLoadFunc )
-    if err != nil {
-      fmt.Printf("Error loading atom tree for %s: %s\n", filename, err.Error() )
-    }
-
-    quicktime.DumpTree( tree )
-  }
+		quicktime.DumpTree(tree)
+	}
 }
